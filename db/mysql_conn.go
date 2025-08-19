71d4a6b564d37445e4a2d2a2b4212d01925314ca
shsw <sae.sahatsawat@gmail.com>
2025-08-19 12:10:21 +0700

    [feat] Add support server public key for remote execution to another MySQL server

--- db/mysql_conn.go
+++ db/mysql_conn.go
@@ -1,18 +1,35 @@
 package db
 
 import (
+	"crypto/rsa"
+	"crypto/x509"
 	"database/sql"
+	"encoding/pem"
 	"fmt"
+	"os"
 
-	_ "github.com/go-sql-driver/mysql"
+	"github.com/go-sql-driver/mysql"
 )
 
-
 // Build DSN, prerequisite check. Return pointer of database or Error.
-func InitializeDB(host *string, port *int, user *string, password *string) (*sql.DB, error) {
+func InitializeDB(host *string, port *int, user *string, password *string, serverPubPath *string) (*sql.DB, error) {
 	var err error
-	// $USER:$PWD@/tcp($HOST:$PORT)/$DATABASE)
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/", *user, *password, *host, *port)
+	var publicKeyName string
+	var dsn string
+	// if public key path is empty -> no use of public key.
+	if *serverPubPath != "" {
+		publicKeyName, err = registerServerPubKey(*serverPubPath)
+		if err != nil {
+			errMsg := fmt.Errorf("fail to register public key: %s", err.Error())
+			return nil, errMsg
+		}
+		// add public key options
+		dsn = fmt.Sprintf("%s:%s@tcp(%s:%d)/?tls=false&serverPubKey=%s", *user, *password, *host, *port, publicKeyName)
+	} else {
+		// no public key options
+		dsn = fmt.Sprintf("%s:%s@tcp(%s:%d)/", *user, *password, *host, *port)
+	}
+
 	conn, err := sql.Open("mysql", dsn)
 	if err != nil {
 		return nil, err
@@ -27,6 +44,35 @@ func InitializeDB(host *string, port *int, user *string, password *string) (*sql
 	}
 
 	return conn, nil
+}
+
+func registerServerPubKey(filePath string) (string, error) {
+	const publicKeyName string = "mysql-enterprise-key"
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		return "", err
+	}
+
+	// Decode file
+	block, _ := pem.Decode(data)
+	if block == nil || block.Type != "PUBLIC KEY" {
+		return "", fmt.Errorf("failed to decode PEM block containing public key")
+	}
+
+	// Parse data into pub structure
+	pub, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		return "", err
+	}
+
+	// Register public key to driver
+	if rsaPubKey, ok := pub.(*rsa.PublicKey); ok {
+		mysql.RegisterServerPubKey(publicKeyName, rsaPubKey)
+	} else {
+		return "", fmt.Errorf("not a RSA public key")
+	}
+
+	return publicKeyName, nil
 }
 
 

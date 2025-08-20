2081f1599f30a576e51b138c324762203e3456d3
shsw <sae.sahatsawat@gmail.com>
2025-08-21 01:18:56 +0700

    [feat] add check engine, duplicate FK

--- features/migration.go
+++ features/migration.go
@@ -49,5 +49,68 @@ func CheckCharSet(conn *sql.DB) ([]models.CharSetObject, error) {
 	}
 
 	return listOfObject, nil
+}
 
+func CheckEngine(conn *sql.DB) ([]models.InformationTableEngine, error) {
+	const statement string = `SELECT table_schema, table_name, engine 
+	FROM information_schema.tables WHERE engine != 'InnoDB' AND 
+	table_schema NOT IN ('mysql','performance_schema','sys','information_schema');`
+
+	var warningTables []models.InformationTableEngine
+	var warningList = []string{"MyISAM", "Memory", "Federated"}
+
+	var warnSet = make(map[string]struct{})
+	for _, w := range warningList {
+		warnSet[w] = struct{}{}
+	}
+
+	// query
+	rows, err := conn.Query(statement)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	// loop through each result
+	for rows.Next() {
+		// scan query results
+		var item models.InformationTableEngine
+		err := rows.Scan(&item.SchemaName, &item.TableName, &item.Engine)
+		if err != nil {
+			return nil, err
+		}
+
+		if _, found := warnSet[item.Engine]; found {
+			warningTables = append(warningTables, item)
+		}
+	}
+
+	return warningTables, nil
+}
+
+func CheckFKDuplication(conn *sql.DB) (int, error) {
+	const statement string = `SELECT COUNT(*) AS constraint_count FROM 
+	INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE CONSTRAINT_TYPE = 'FOREIGN KEY' 
+	GROUP BY CONSTRAINT_SCHEMA, CONSTRAINT_NAME;`
+	var FKDuplicationCounts int = 0
+	// query
+	rows, err := conn.Query(statement)
+	if err != nil {
+		return 0, err
+	}
+	defer rows.Close()
+	// loop through each result
+	for rows.Next() {
+		// scan query results
+		var FKCount int;
+		err := rows.Scan(&FKCount)
+		if err != nil {
+			return 0, err
+		}
+
+		if FKCount > 1 {
+			FKDuplicationCounts += 1
+		}
+	}
+
+	return FKDuplicationCounts, nil
 }
